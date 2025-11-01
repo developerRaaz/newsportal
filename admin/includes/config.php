e2db8cf5becd363647ee9796f0c71f35b6a316cb
sarfaraz <sarfaraz123@gmail.com>
2025-11-01 12:23:01 +0530

    Updated code

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,9 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
+define('DB_SERVER','sql12.freesqldatabase.com');
+define('DB_USER','sql12805574');
+define('DB_PASS' ,'CYlnqRj8IS');
+define('DB_NAME','sql12805574');
+define('DB_PORT',3306);
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
