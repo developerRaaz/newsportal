ec87d31df640037e50a7c02934d7de11dd3e9fdb
sarfaraz <sarfaraz123@gmail.com>
2025-11-01 13:01:00 +0530

    Updated code

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -3,7 +3,7 @@ define('DB_SERVER','sql12.freesqldatabase.com');
 define('DB_USER','sql12805574');
 define('DB_PASS' ,'CYlnqRj8IS');
 define('DB_NAME','sql12805574');
-define('DB_PORT',3306);
+
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
